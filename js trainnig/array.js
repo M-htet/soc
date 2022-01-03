b87aebc707b4b62d505c8f5582f71f1149491e16
Min Htet Aung <64087476+M-htet@users.noreply.github.com>
2022-01-03 22:17:12 +0630

    Add files via upload

---
+++ js trainnig/array.js
@@ -0,0 +1,61 @@
+//  const people = "MgMg";
+// let array = new Array ("mgmg", "agag", "soe", 20, 24, true, false);
+// console.log(array);
+
+// let arr = ["apple", "pine", "tiny", undefined];
+// console.log(arr);
+
+// let sira = ["apple", "pine", "tiny", undefined, "mgmg", "agag", "soe", 20, 24, true, false];
+// // console.log(sira.length);
+// console.log(sira [sira.length-3]);
+
+// arr[0] = "koko"
+// console.log(arr);
+
+// arr.push("aa")
+// console.log(arr);
+
+// arr.unshift("256")
+// console.log(arr);
+
+
+// delate end word
+// arr.pop()
+// console.log(arr);
+
+// // delate start word
+// arr.shift()
+// console.log(arr);
+
+// let anti = "koko";
+// console.log(Array.isArray(sira));
+// console.log(Array.isArray(anti));
+
+// console.log(anti.indexOf("koko"));
+
+// console.log(arr.splice(1));
+// console.log(arr);
+ 
+// const mini = arr.join("//")
+// console.log(mini);
+
+// const omg = "thu thu";
+// const mog = ["so", "auto", "ko", ",oe"];
+// const gom = [omg + mog];
+// console.log(gom);
+
+// const ogm = [...omg, ...mog];
+// console.log(ogm);
+
+
+// const arr = ["apple", "pine", "tiny", undefined]
+// const [a,b,...c] = arr;
+// console.log(a);
+
+const craso = [["apple", "soe", 1, 11,], ["ko", "thu", 54, 22], ["moe", 49, true]];
+console.log(craso[2][2]);
+
+console.log(craso.length);
+// console.log(Array.length(craso));
+
+console.log("");
---
+++ js trainnig/object.js
@@ -0,0 +1,3 @@
+// console.log(Math.round(2.5));
+// console.log(Math.floor(2.9));
+// console.log(Math.abs(-10.55444));
